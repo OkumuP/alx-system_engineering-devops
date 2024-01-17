c8e9792d4192bb9de5c74f87199d337c753a7c3a
OkumuP <okumupita@gmail.com>
2024-01-17 02:11:01 +0000

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit
+queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit
 """
 
 from requests import get
